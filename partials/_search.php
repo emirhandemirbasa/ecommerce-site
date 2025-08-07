75312bfd88eada64ed14b028116ba69ae1fe9778
Emirhan Demirbaşa <58663784+emirhandemirbasa@users.noreply.github.com>
2025-08-07 22:53:26 +0300

    inex.php için site parçaları oluşturuldu
    Anasayfa parçaları

---
+++ partials/_search.php
@@ -0,0 +1,12 @@
+<div class="container-fluid" style="margin-top: 80px;">
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-auto">
+      <form class="d-flex justify-content-center" style="width: 700px; max-width: 100%; margin: auto;">
+        <input class="form-control me-2" type="search" placeholder="Ürün ara..." name="q" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">
+          <i class="bi bi-search"></i>
+        </button>
+      </form>
+    </div>
+  </div>
+</div>
